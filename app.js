d4337582fcfe3993dfd6244006c1e02138969e00
ayoub404-mak <ayoub151539@gmail.com>
2026-04-16 19:12:27 +0200

    add partials(nav+footer+head)

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
  const  express = require('express');
+const { title } = require('node:process');
 
  // express app
  const app = express();
@@ -11,17 +12,23 @@
 app.listen(3000);
 
 app.get('/', (req,res)=>{
+
+    const blogs = [
+    {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+  ];
     
     //res.send('<p> home page </p>');
     //res.sendFile('./views/index.html',{root:__dirname });
-    res.render('index');
+    res.render('index',{title: 'Home', blogs:blogs});
 });
 
 app.get('/about', (req,res)=>{
     
     // res.sendFile('./views/about.html',{root:__dirname });
     //res.send('<p> about page </p>');
-    res.render('about');
+    res.render('about',{title:'About'});
 });
 
 
@@ -29,7 +36,7 @@ app.get('/about', (req,res)=>{
 
 // redirects
 app.get('/blogs/create',(req,res) =>{
-    res.render('create');
+    res.render('create',{title:'Creat a new Blog'});
 })
 
 
@@ -37,5 +44,5 @@ app.get('/blogs/create',(req,res) =>{
 app.use((req,res) => {
     
     //res.status(404).sendFile('./views/404.html',{root:__dirname })
-    res.status(404).render('404');
+    res.status(404).render('404',{title:'404'});
 })
