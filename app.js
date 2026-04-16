09d5d4a814fe9a99c2657c8cba532c22e7522592
ayoub404-mak <ayoub151539@gmail.com>
2026-04-16 17:13:48 +0200

    first code ejs

--- app.js
+++ app.js
@@ -13,23 +13,29 @@ app.listen(3000);
 app.get('/', (req,res)=>{
     
     //res.send('<p> home page </p>');
-    res.sendFile('./views/index.html',{root:__dirname });
+    //res.sendFile('./views/index.html',{root:__dirname });
+    res.render('index');
 });
 
 app.get('/about', (req,res)=>{
-     res.sendFile('./views/about.html',{root:__dirname });
+    
+    // res.sendFile('./views/about.html',{root:__dirname });
     //res.send('<p> about page </p>');
+    res.render('about');
 });
+
 
 
 
 // redirects
-app.get('/about-us',(req, res) =>{
-    res.redirect('/about'); 
-});
+app.get('/blogs/create',(req,res) =>{
+    res.render('create');
+})
 
 
 //404 page
 app.use((req,res) => {
-    res.status(404).sendFile('./views/404.html',{root:__dirname })
+    
+    //res.status(404).sendFile('./views/404.html',{root:__dirname })
+    res.status(404).render('404');
 })
