a45ec626b6631d112967088885439dbac06369b4
ayoub404-mak <ayoub151539@gmail.com>
2026-04-19 13:44:03 +0200

    Express router + MVC archt

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const  express = require('express');
 //const { title } = require('node:process');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-const Blog = require('./models/blog');
-const { result } = require('lodash');
 
+const { result } = require('lodash');
+const blogRoutes = require('./routes/blogRoutes');
 
 
  // express app
@@ -91,61 +91,7 @@ app.get('/about', (req,res)=>{
 
 
 // redirects----blog routes
-
-app.get('/blogs', (req, res) => {
-    Blog.find().sort({createdAt: -1})
-    .then((result) => {
-        res.render('index' , { title: 'All Blogs', blogs: result})
-
-    })
-    .catch((err) => {
-        console.log(err);
-    })
-});
-
-
-app.post('/blogs', (req, res) => {
-    const blog = new Blog(req.body);
-
-    blog.save()
-    .then((result) => {
-        res.redirect('/blogs');
-    })
-    .catch((err) => {
-        console.log(err);
-    })
-});
-
-
-app.get('/blogs/:id', (req, res) => {
-    const id = req.params.id;
-    Blog.findById(id)
-    .then(result => {
-        res.render('details', {blog: result, title: 'Blog Details'});
-    })
-    .catch(err => {
-        console.log(err);
-    });
-})
-
-
-app.delete('/blogs/:id', (req, res) => {
-    const id = req.params.id;
-
-    Blog.findByIdAndDelete(id)
-    .then(result => {
-        res.json({ redirect: '/blogs'})
-    }) 
-    .catch(err => {
-        console.log(err);
-    })
-})
-
-
-app.get('/blogs/create',(req,res) =>{
-    res.render('create',{title:'Creat a new Blog'});
-})
-
+app.use('/blogs' ,blogRoutes);
 
 //404 page
 app.use((req,res) => {
